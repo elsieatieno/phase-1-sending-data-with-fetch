548e573630669ff3146e155807be9d6be77e3435
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-06-26 09:16:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email){
+ const userInfo = { 
+ name: name,
+ mail: email
+
+}
+}
+const fetchInfo = {
+    method: 'POST',
+    header: {
+        'Content-Type': 'application/json',
+        Accept : 'application/json'
+    },
+    body: JSON.stringify(userInfo),
+  };
+
+  fetch("http://localhost:3000/users", fetchInfo)
+  .then(res => res.json())
+
+  .then(data => console.log(data))
+
+  .catch(function (error){
+  alert('Unauthorized Access');
+  console.log(error.message);
+})
+
